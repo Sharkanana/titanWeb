2d6ba771638c4f0a7b22948ed25d999c48cb4666
Helfert <john.helfert@virtualinstruments.com>
2015-02-05 20:04:29 -0500

    Drawing the board

--- app/view/main/MainController.js
+++ app/view/main/MainController.js
@@ -5,17 +5,38 @@ Ext.define('TitanWeb.view.main.MainController', {
     extend: 'Ext.app.ViewController',
 
     requires: [
+        'TitanWeb.view.Space'
     ],
 
     alias: 'controller.main',
 
     startNewGame: function () {
 
+        var me = this;
+
+        Ext.Ajax.request({
+            url: 'resources/boards/Standard.json',
+            success: function(results) {
+                me.drawBoard(Ext.decode(results.responseText));
+            }
+        });
+    },
+
+    drawBoard: function(board) {
         var me = this,
             gameBoard = me.lookupReference('gameBoard'),
-            surface = gameBoard.getSurface();
+            surface = gameBoard.getSurface(),
+            radius = 25,
+            scale = 55,
+            padding = 30;
 
-
+        Ext.each(board.spaces, function(space) {
+            var newSpace = surface.add(Ext.create('TitanWeb.view.Space', Ext.apply(space, {
+                r: radius,
+                myScale: scale,
+                myPadding: padding
+            })));
+        });
 
         gameBoard.renderFrame();
     },
--- app/view/movement/Space.js
+++ app/view/movement/Space.js
@@ -3,9 +3,19 @@
  * A space on the main movement game board
  */
 Ext.define('TitanWeb.view.Space', {
-    extend: 'Ext.draw.sprite.Sprite',
-    type: 'circle',
-    fill: '#ff0',
+    extend: 'Ext.draw.sprite.Circle',
+    alias: 'sprite.space',
+    type: 'space',
+
+    /**
+     * scale for space spacing
+     */
+    myScale: null,
+
+    /**
+     * padding from borders of draw container
+     */
+    myPadding: null,
 
     /**
      * type of terrain for this space
@@ -17,10 +27,30 @@ Ext.define('TitanWeb.view.Space', {
      */
     orientation: 'up',
 
-    /**
-     * Space's neighbors
-     */
-    verticalNeighbor: null,
-    leftNeighbor: null,
-    rightNeighbor: null
+    constructor: function(config) {
+        var me = this;
+
+        config.x = config.xPos * config.myScale + config.myPadding;
+        config.y = config.yPos * config.myScale + config.myPadding;
+        config.fillStyle = TitanWeb.view.Space.TERRAIN_COLOR_MAP[config.terrain];
+        config.strokeStyle = 'black';
+
+        me.callParent(arguments);
+    },
+
+    statics: {
+        TERRAIN_COLOR_MAP: {
+            jungle: 'darkgreen',
+            marsh: 'purple',
+            plains: 'yellow',
+            brush: '#996633',
+            hills: '#663300',
+            tundra: 'lightblue',
+            mountains: 'red',
+            desert: 'orange',
+            tower: 'white',
+            swamp: 'blue',
+            woods: 'lightgreen'
+        }
+    }
 });
